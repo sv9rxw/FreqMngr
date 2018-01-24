41563d3bbba30c2eceef54bf835f577f63a427dc
sv9rxw <sv9rxw@hotmail.com>
2018-01-24 21:02:48 +0200

    Added DbServer interface, DbServiceMock and  IsInDesignTime property in main viewmodel for differenciating between design-time and run-time.

---
+++ FreqMngr.WPF/FreqMngr/Services/DbServiceMock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreqMngr.Models;
+using System.Collections.ObjectModel;
+
+namespace FreqMngr.Services
+{
+    public class DbServiceMock : IDbService
+    {
+        public IEnumerable<Group> GetAllGroups()
+        {
+            ObservableCollection<Group> groupList = new ObservableCollection<Group>();
+            groupList.Add(new Group(1, "All", 0));
+            groupList.Add(new Group(2, "HF", 1));
+            groupList.Add(new Group(3, "Mil-Gov", 2));
+            groupList.Add(new Group(4, "DISA Mystic Star", 3));
+            groupList.Add(new Group(5, "USAF HFGCS", 3));
+            groupList.Add(new Group(6, "Number Stations", 3));
+            groupList.Add(new Group(7, "Aero", 2));
+            groupList.Add(new Group(8, "VOLMETs", 7));
+            groupList.Add(new Group(9, "MWARA", 7));
+            groupList.Add(new Group(10, "MWARA NAT-A", 9));
+            return groupList;
+        }
+
+        public IEnumerable<Freq> GetFreqs(Group group)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbServiceMock(String path)
+        {
+            Console.WriteLine("Design-time DbServiceMock");
+        }
+
+        public bool Connect()
+        {
+            return true;
+        }
+    }
+}
--- FreqMngr.WPF/FreqMngr/ViewModels/BaseViewModel.cs
+++ FreqMngr.WPF/FreqMngr/ViewModels/BaseViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FreqMngr.ViewModels
 {
@@ -15,6 +16,19 @@ namespace FreqMngr.ViewModels
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        /// <summary>
+        /// Indicates if we are in design mode or at real runtime
+        /// </summary>
+        public bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
             }
         }
     }
