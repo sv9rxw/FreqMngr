40a80431890f0be9cbb3312bd71f1d4b3c4e87d1
sv9rxw <sv9rxw@hotmail.com>
2018-01-24 20:21:00 +0200

    Re-implemented form scratch using MVVM techniques and Expression Blend

---
+++ FreqMngr.WPF/FreqMngr/Models/Freq.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FreqMngr.Models
+{    
+    public class Freq : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private NumberFormatInfo NumberFormat { get; set; } = null;        
+
+        private String _Name;
+        public String Name
+        {
+            get; set;
+        }
+
+        private double FrequencyInKHz { get; set; }
+
+        public String Frequency
+        {
+            get; set;
+        }
+
+        private double BandwidthInKHz { get; set; }
+        public String Bandwidth
+        {
+            get; set;
+        }
+        
+        public String Modulation
+        {
+            get; set;
+        }
+        
+        public String ModulationType
+        {
+            get; set;
+        }
+
+        public String Protocol
+        {
+            get; set;
+        }
+
+        public String Country
+        {
+            get; set;
+        }
+
+        public String User
+        {
+            get; set;
+        }
+        
+
+        public String Coordinates
+        {
+            get; set;
+        }
+
+        public String Description
+        {
+            get; set;
+        }
+
+        public String URLs
+        {
+            get; set;
+        }
+
+        private String QSLStr
+        {
+            get; set;
+        }
+
+        public bool QSL
+        {
+            get; set;
+        }
+
+
+
+
+        public Freq()
+        {
+
+
+            this.NumberFormat = new NumberFormatInfo();
+            this.NumberFormat.NumberDecimalSeparator = ".";
+            this.NumberFormat.NumberGroupSeparator = ",";
+        }
+
+    }
+}
---
+++ FreqMngr.WPF/FreqMngr/Services/DbService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+using System.Diagnostics;
+
+using FreqMngr.Models;
+using System.Collections.ObjectModel;
+
+namespace FreqMngr.Services
+{
+    public class DbService
+    {
+        private static String TABLEGROUPS_CLM_ID = "Id";
+        private static String TABLEGROUPS_CLM_NAME = "Name";
+        private static String TABLEGROUPS_CLM_PARENTID = "ParentId";
+
+        private String _MdfDbFilePath;
+        private String _ConnectionString = null;
+        private SqlConnection _SqlConnection = null;
+
+
+        private bool _Connected = false;
+        public bool Connected
+        {
+            get
+            {
+                return _Connected;                        
+            }
+        }
+
+        public DbService(String mdfDbFilePath)
+        {
+            if (String.IsNullOrWhiteSpace(mdfDbFilePath)) throw new ArgumentException("mdfDbFilePath");
+            _MdfDbFilePath = mdfDbFilePath;
+
+            //TODO: construct connection string            
+            _ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"" + _MdfDbFilePath +"\"; Integrated Security = True";
+        }
+
+        public bool Connect()
+        {
+            //TODO: connect to MDF file database using SQL client stuff
+            _SqlConnection = new SqlConnection(_ConnectionString);
+            _SqlConnection.Open();
+            _Connected = true;
+            Debug.WriteLine("Connected");
+
+            return true;
+        }
+
+        
+        public IEnumerable<Group> GetAllGroups()
+        {
+            if (_Connected == false) throw new Exception("Not connected to any database");
+
+            SqlCommand cmd = new SqlCommand("select * from TableGroups", _SqlConnection);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            ObservableCollection<Group> groupList = new ObservableCollection<Group>();
+            while (reader.Read())
+            {
+                int groupId = (int)reader[TABLEGROUPS_CLM_ID];
+                String groupName = (String)reader[TABLEGROUPS_CLM_NAME];
+                int groupParentId = (int)reader[TABLEGROUPS_CLM_PARENTID];
+                Group group = new Group(groupId, groupName, groupParentId);
+                groupList.Add(group);
+            }
+            reader.Close();
+            return groupList;
+        }
+
+        public Group GetRootGroup()
+        {
+            if (_Connected==false) throw new Exception("Not connected to any database");
+            Group root = null;
+
+            IEnumerable<Group> allGroupsList = GetAllGroups();
+            
+
+                                                            
+            return root;
+        }
+
+        public IEnumerable<Freq> GetFreqs(Group group)
+        {
+            throw new NotImplementedException(nameof(GetFreqs));
+        }
+        
+    }
+}
