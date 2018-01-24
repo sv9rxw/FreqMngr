117572a30501264930301be58a67d02fef84f61b
sv9rxw <sv9rxw@hotmail.com>
2018-01-24 20:24:25 +0200

    Added missing model files

---
+++ FreqMngr.WPF/FreqMngr/Models/Group.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FreqMngr.Models
+{
+    public class Group :  INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private Group _Parent = null;
+        public Group Parent
+        {
+            get
+            {
+                return _Parent;
+            }
+            set
+            {
+                if (value == _Parent)
+                    return;
+
+                _Parent = value;
+                OnPropertyChanged(nameof(Parent));
+            }
+        }
+
+        private int _Id;
+
+        private int _ParentId;
+
+        private String _Name = null;
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (value == _Name)
+                    return;
+
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }                
+        }
+
+
+        public Group(String name)
+        {
+            if (String.IsNullOrWhiteSpace(name)) throw new ArgumentException("name");
+            _Name = name;
+        }
+
+        public Group(int id, String name, int parentId) 
+            :this(name)
+        {
+            _Id = id;
+            _ParentId = parentId;
+        }
+
+        public Group(String name, Group parent)
+            : this(name)
+        {
+            _Parent = parent;
+        }
+
+        private ObservableCollection<Group> _Children = null;
+
+        private ObservableCollection<Group> Children       
+        {
+            get
+            {
+                return _Children;
+            }
+            set
+            {
+                if (value == _Children)
+                    return;
+
+                _Children = value;
+                OnPropertyChanged(nameof(Children));
+            }
+        }                   
+        
+
+        public override string ToString()
+        {
+            return ("[" + _Id.ToString() + ", " + Name + ", " + _ParentId.ToString() + "]");
+        }
+    }
+}
---
+++ FreqMngr.WPF/FreqMngr/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreqMngr.Models;
+using FreqMngr.Services;
+
+namespace FreqMngr.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Service = new DbService(@"C:\Users\Dirty Harry\Source\Repos\FreqMngr\FreqMngr.WPF\FreqMngr\FreqDB.mdf");
+            Service.Connect();
+            var mlkia = Service.GetAllGroups();
+            TestPrintGroups(mlkia);
+        }
+
+        private void TestPrintGroups(IEnumerable<Group> groupList)
+        {
+            foreach(Group group in groupList)            
+                Console.WriteLine(group.ToString());            
+        }
+
+        private DbService Service { get; set; } = null;
+
+        private ObservableCollection<Freq> _Freqs;
+        public ObservableCollection<Freq> Freqs
+        {
+            get
+            {
+                return _Freqs;
+            }
+            set
+            {
+                if (value == _Freqs)
+                    return;
+
+                _Freqs = value;
+                OnPropertyChanged(nameof(Freqs));
+            }
+        }
+
+        private Group _RootGroup = null;
+        public Group RootGroup
+        {
+            get
+            {
+                return _RootGroup;
+            }
+            set
+            {
+                if (value == _RootGroup)
+                    return;
+
+                _RootGroup = value;
+                OnPropertyChanged(nameof(RootGroup));
+            }
+        }
+
+        private Group _ActiveGroup = null;
+        public Group ActiveGroup
+        {
+            get { return _ActiveGroup; }
+            set
+            {
+                if (value == _ActiveGroup)
+                    return;
+
+                _ActiveGroup = value;
+                OnPropertyChanged(nameof(ActiveGroup));
+            }
+        }
+
+
+    }
+}
